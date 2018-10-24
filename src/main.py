0bbfdf9c6c27453ed91d7e2f450895e8fcaaf156
clussier <lussierc@allegheny.edu>
2018-10-24 10:31:27 -0400

    Updates the code formatting and comments.

--- src/main.py
+++ src/main.py
@@ -1,67 +1,51 @@
-
+## Refined Notepad Python Code:
 # Imports:
 from tkinter import *
 import tkinter.filedialog
 
-class TextEditor:
-
-    # Function for Quitting the program:
+class TextEditor: #set class
     @staticmethod
-    def quit_app(event = None):
-        root.quit()
+    def quit_app(event = None): # function for quitting the program
+        root.quit() # quits the program
 
-    def save_file(self, event = None):
-
+    def save_file(self, event = None): # function for saving files
         # Opens the save as dialog box
         file = tkinter.filedialog.asksaveasfile(mode = 'w')
         if file != None:
             data = self.text_area.get('1.0', END + '-1c')
-            file.write(data)
-            file.close()
+            file.write(data) # saves the data/info to the file
+            file.close() # closes the file
 
-    def open_file(self, event = None):
-        txt_file = tkinter.filedialog.askopenfilename(parent = root, initialdir = '')
+    def open_file(self, event = None): # function for openign a file
+        txt_file = tkinter.filedialog.askopenfilename(parent = root, initialdir = '') # does variety of tasks, sets the initial directory.
         if txt_file:
             self.text_area.delete(1.0, END)
             with open(txt_file) as _file:
                 self.text_area.insert(1.0, _file.read())
                 root.update_idletasks()
 
-    def __init__(self, root):
+    def __init__(self, root): # initializes the notepad program
         self.text_to_write = ""
-        root.title("Text Editor")
-        root.geometry("600x550")
-        frame = Frame(root, width = 600, height = 550)
-        scrollbar = Scrollbar(frame)
-        self.text_area = Text(frame, width = 600, height = 550, yscrollcommand = scrollbar.set, padx = 10, pady = 10)
+        root.title("Refined Notepad") # sets the program title
+        root.geometry("600x550") # sets the window size for program
+        frame = Frame(root, width = 600, height = 550) # sets the frame size for program
+        scrollbar = Scrollbar(frame) # creates a scrollbar for scrolling through text
+        self.text_area = Text(frame, width = 600, height = 550, yscrollcommand = scrollbar.set, padx = 10, pady = 10) # sets area where text will be, with scrollbar
         scrollbar.config(command = self.text_area.yview)
-        scrollbar.pack(side = "right", fill = "y")
+        scrollbar.pack(side = "right", fill = "y") # sets scrollbar location
         self.text_area.pack(side = "left", fill = "both", expand = True)
         frame.pack()
 
-        the_menu = Menu(root)
+        the_menu = Menu(root) #creates menu
+        file_menu = Menu(the_menu, tearoff = 0) #creates menu
 
-        file_menu = Menu(the_menu, tearoff = 0)
-
-        # Add items to the menu that show when clicked
-        # compound allows you to add an image
-        file_menu.add_command(label = "Open", command = self.open_file)
-        file_menu.add_command(label = "Save", command = self.save_file)
-
-        # Add a horizontal bar to group similar commands
-        file_menu.add_separator()
-
-        # Call for the function to execute when clicked
-        file_menu.add_command(label = "Quit", command = self.quit_app)
-
-        # Add the pull down menu to the menu bar
-        the_menu.add_cascade(label = "File", menu = file_menu)
-
-        # Display the menu bar
-        root.config(menu = the_menu)
+        file_menu.add_command(label = "Open", command = self.open_file) # add item/command to the menu
+        file_menu.add_command(label = "Save", command = self.save_file) # add item/command to the menu
+        file_menu.add_separator() # used for grouping commands
+        file_menu.add_command(label = "Quit", command = self.quit_app) # menu item that allows user to quit the program
+        the_menu.add_cascade(label = "File", menu = file_menu)         # Add the pull down menu to the menu bar
+        root.config(menu = the_menu) # displays the menu bar
 
 root = Tk()
-
 text_editor = TextEditor(root)
-
 root.mainloop()
